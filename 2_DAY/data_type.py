b82d8afe32c2306ec0db44a7ddd2defd69abd07c
VedantGore07 <vedantgore713@gmail.com>
2025-11-28 19:57:45 +0530

    data types, operators

---
+++ 2_DAY/data_type.py
@@ -0,0 +1,69 @@
+#string datatype
+#literal assignment
+
+first_name = "Virat"
+last_name = "Kohli"
+print(type(first_name))
+print(type(last_name))
+
+full_name = first_name+ " " +last_name+ "!"
+print(full_name)
+
+
+
+#####################
+
+decade = str(1980)
+print(type(decade))
+print(decade)
+
+statement = "I like rock music from the " +decade+ "s"
+print(statement)
+
+# sentence = "I\'m Virat \t Welcome \n to my session"
+# print(sentence)
+
+
+################################
+
+sentence = "i am Virat Welcome to my Python session"
+print(sentence.lower())
+print(sentence.upper())
+print(sentence.title())
+print(sentence.replace('Python', 'Python3'))
+print(len(sentence))
+print(sentence.lstrip())
+
+
+
+#################
+
+price = 100
+
+print(type(price))
+
+
+gpa = 3.28
+print(type(gpa))
+print(abs(gpa))
+print(abs(gpa*-1))
+print(round(gpa,1))
+print(round(gpa))
+
+
+
+# zipcode = int("New York")
+# print(zipcode)
+
+
+
+
+############################################3
+
+# List
+
+grocery_list = ['milk', 'butter', 'bread']
+print('milk' in grocery_list)
+
+
+
